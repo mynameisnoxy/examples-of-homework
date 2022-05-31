bfba90e66663947e615a859f14a675f1fb78b269
Nikita Goremykin <99750681+mynameisnoxy@users.noreply.github.com>
2022-05-31 05:06:25 +0300

    Update Homework_1_SQL.sql

--- Homework_1_SQL.sql
+++ Homework_1_SQL.sql
@@ -83,4 +83,4 @@ select id, name, created_on from students
 order by created_on;
 --32. Вывести id пользователя, имя, дату создания пользователя. Отсортировать по порядку убывания даты добавления пользоватлеля.
 select id, name, created_on from students 
-order by created_on desc ;
+order by created_on desc.
